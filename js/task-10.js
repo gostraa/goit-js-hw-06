ec017722ba3165bb4b10b80bfb3c17225e4161c7
gostraa <mcherkashina77@gmail.com>
2023-07-19 17:03:30 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -6,4 +6,43 @@ function getRandomHexColor() {
 
 
 
+const input = document.querySelector('[type="number"]');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const container = document.querySelector("#boxes");
 
+
+createBtn.addEventListener("click", () => {
+  
+  createBoxes(input.value);
+
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(number) {
+  const newArr = [];
+  for (let i = 0; i < number; i += 1) {
+    
+    const newDiv = `<div style = "width:${30 + 10 * i + "px"};
+     height: ${30 + 10 * i + "px"}; 
+     background-color: ${getRandomHexColor()}"></div>`
+    newArr.push(newDiv);
+
+
+    // const newDiv = document.createElement("div");
+    // newDiv.style.width = 30 + 10 * i + "px";
+    // newDiv.style.height = 30 + 10 * i + "px";
+    // newDiv.style.backgroundColor = getRandomHexColor();
+    // container.insertAdjacentElement("beforeend",newDiv);
+    
+  }
+  
+  container.insertAdjacentHTML("beforeend",newArr.join(""))
+}
+
+function destroyBoxes() {
+ 
+ container.innerHTML = "";
+  
+}
